5813dd7018bec76a61b1aaec9f33e3e457488da1
carlamp <cmolins@elisava.net>
2021-04-14 17:04:27 +0200

    Day 11 sensors

---
+++ week 10/Sensors/Day 11/D11_S2/D11_S2.ino
@@ -0,0 +1,22 @@
+//www.elegoo.com
+//2016.06.13
+
+//Reed switch analog reading
+int  sensorPin  =  A0;     // select the input  pin for  the potentiometer 
+int  ledPin  =  13;   // select the pin for  the LED
+int  sensorValue =  0;  // variable to  store  the value  coming  from  the sensor
+
+void setup()
+{
+pinMode(ledPin,OUTPUT);
+Serial.begin(9600);
+}
+void loop(){
+sensorValue =  analogRead(sensorPin);
+digitalWrite(ledPin,  HIGH); 
+delay(sensorValue); 
+digitalWrite(ledPin,  LOW);
+delay(sensorValue);
+Serial.println(sensorValue,  DEC);
+}
+
